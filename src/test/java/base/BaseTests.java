7d0e694b2d20a786230d8f382ef9d53ca6233d5b
Christian Baumann <chriss713@gmail.com>
2021-03-02 20:19:29 +0100

    Adding page objects

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,18 +3,20 @@ package base;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
-    public void setUp(){
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
         //1 - Maximize the window
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
 
         //2 - Fullscreen mode
         //driver.manage().window().fullscreen();
@@ -23,11 +25,12 @@ public class BaseTests {
         //Dimension size = new Dimension(375, 812);
         //driver.manage().window().setSize(size);
 
-        System.out.println(driver.getTitle());
+        homePage = new HomePage(driver);
+
         driver.quit();
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         BaseTests test = new BaseTests();
         test.setUp();
     }
