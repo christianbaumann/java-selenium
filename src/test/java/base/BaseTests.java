8bb3efb8a58c07aab2bb0bd20d772983ff4fadc4
Christian Baumann <chriss713@gmail.com>
2021-03-02 19:58:44 +0100

    Added BaseTests class

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,34 @@
+package base;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+        //1 - Maximize the window
+        driver.manage().window().maximize();
+
+        //2 - Fullscreen mode
+        //driver.manage().window().fullscreen();
+
+        //3 - Specific width (show Chrome iPhoneX emulator)
+        //Dimension size = new Dimension(375, 812);
+        //driver.manage().window().setSize(size);
+
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    public static void main(String args[]){
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
