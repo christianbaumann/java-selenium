7bea35a159fd5a0025d68c3168913b1fac97f198
Christian Baumann <chriss713@gmail.com>
2021-03-07 12:07:34 +0100

    Taking screenshots only in case of test failure

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
 import utils.WindowManager;
@@ -61,14 +62,16 @@ public class BaseTests {
     }
 
     @AfterMethod
-    public void takeScreenshot() {
-        var camera = (TakesScreenshot) driver;
-        File screenshot = camera.getScreenshotAs(OutputType.FILE);
-        File destinationFile = new File("resources/screenshots/test.png");
-        try {
-            FileUtils.copyFile(screenshot, destinationFile);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            File destinationFile = new File("resources/screenshots/" + result.getName() + ".png");
+            try {
+                FileUtils.copyFile(screenshot, destinationFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
